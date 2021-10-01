a4f8c4b64f707ef4cbb93758daf90ef1c1fbc838
Bess <60241300+BessPtck@users.noreply.github.com>
2021-10-01 12:11:34 -0700

    Set Env file for OpenCV
    Assumes OpenCV is installed in the default directory

---
+++ setenv.sh
@@ -0,0 +1,3 @@
+export CPATH=$CPATH:"/usr/local/include/":"/usr/local/include/opencv4/"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"/usr/local/include/":"/usr/local/include/opencv4/"
+export PATH=$PATH:"/usr/local/include/opencv4/"
